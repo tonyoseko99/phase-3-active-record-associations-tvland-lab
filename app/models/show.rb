41604e9cc422bf03106ff33e260c876d486b6def
Tonny Oseko <tonnyoseko0@gmail.com>
2022-08-31 10:15:10 +0300

    done

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        list = []
+        self.actors.each do |actor|
+            list << actor.full_name
+        end
+        list
+    end
   
 end
